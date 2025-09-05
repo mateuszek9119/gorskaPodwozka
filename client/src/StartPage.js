93261d261184e4c34a656cfce711148a49d88a2d
mateusz <mateusz@gmail.com>
2025-09-05 15:59:58 +0200

    change view display font description and contact

--- client/src/StartPage.js
+++ client/src/StartPage.js
@@ -141,7 +141,7 @@ function StartPage() {
 
   limit={3}
   style={{
-    marginBottom: '84vh',
+    marginBottom: '80vh',
     zIndex: 9999,         // Upewniamy się, że toast jest na wierzchu
     pointerEvents: 'none', // Upewniamy się, że toast nie blokuje interakcji
   }}
