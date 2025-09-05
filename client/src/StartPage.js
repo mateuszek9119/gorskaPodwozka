697dd57cf814af77790d21c547cc23b7df1bc007
mateusz <mateusz@gmail.com>
2025-09-05 21:33:32 +0200

    change view display font description and contact

--- client/src/StartPage.js
+++ client/src/StartPage.js
@@ -154,7 +154,7 @@ function StartPage() {
   hideProgressBar={true}
   limit={3}
   style={{
-    marginTop: '28vh',
+    marginTop: '28.5vh',
     zIndex: 9999,         // Upewniamy się, że toast jest na wierzchu
     pointerEvents: 'none', // Upewniamy się, że toast nie blokuje interakcji
   }}
